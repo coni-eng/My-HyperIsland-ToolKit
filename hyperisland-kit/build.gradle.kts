48ab37481c10775c2e4ddc3429fd795cff9e8b51
bekir <coniconilesh@gmail.com>
2026-01-05 07:17:16 +0300

    Starting to make my own app by making First changelog from D4vidDf's publish under APACHE 2.0 LICENSE. 'Disable publishing config for local toolkit composite build'

--- hyperisland-kit/build.gradle.kts
+++ hyperisland-kit/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.23"
-    id("com.vanniktech.maven.publish") version "0.35.0" // Use a recent version
+
 
 }
 android {
@@ -41,42 +41,7 @@ android {
     }
 }
 
-mavenPublishing {
 
-
-    coordinates("io.github.d4viddf", "hyperisland_kit", "0.4.4")
-
-    pom {
-        name = "HyperIsland ToolKit"
-        description = "A simple Kotlin library for creating notifications on Xiaomi's HyperIsland. Abstracts away the complex JSON and Bundle-linking, allowing you to build HyperIsland notifications with a few lines of Kotlin."
-        inceptionYear = "2025"
-        url = "https://github.com/D4vidDf/HyperIsland-ToolKit"
-        licenses {
-            license {
-                name = "The Apache License, Version 2.0"
-                url = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-                distribution = "https://www.apache.org/licenses/LICENSE-2.0.txt"
-            }
-        }
-        developers {
-            developer {
-                id = "d4viddf"
-                name = "D4vidDF"
-                email="d4viddf@d4viddf.com"
-                organization = "D4vidDF"
-                organizationUrl = "https://d4viddf.com"
-                url = "https://d4viddf.com"
-            }
-        }
-        scm {
-            url = "https://github.com/D4vidDf/HyperIsland-ToolKit"
-            connection = "scm:git:git://github.com/D4vidDf/HyperIsland-ToolKit.git"
-            developerConnection = "scm:git:ssh://git@github.com/D4vidDf/HyperIsland-ToolKit.git"
-        }
-    }
-    publishToMavenCentral()
-    signAllPublications()
-}
 
 dependencies {
     implementation(libs.androidx.core.ktx)
